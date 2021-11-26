3f84a34929dc48f32dec120fefdeb500893e3d5b
thibzzz09 <thibault.richel2@gmail.com>
2021-11-26 11:21:51 +0100

    cognito config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-3_Y4KHaS3KG',
+        userPoolClientId: '6ucsggve3s94b4us0vuqa1jf6h',
+        region: 'eu-west-3' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
